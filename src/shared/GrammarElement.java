3499c36e3e07111f7e8a1cced4ca7203130eba43
Mark Fulbright <mfulbright777@gmail.com>
2015-04-16 00:38:54 -0500

    Removed isNonterminal() from the GrammarElement interface

--- src/shared/GrammarElement.java
+++ src/shared/GrammarElement.java
@@ -1,5 +1,6 @@
 package shared;
 
 public interface GrammarElement {
-    public boolean isNonterminal();
+    // This is a blank interface, just used to give multiple classes a common
+    // name
 }
