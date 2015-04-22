919bef94717cede1a0e0d39bd70ba87e4b12103e
Mark Fulbright <mfulbright777@gmail.com>
2015-04-22 15:30:19 -0500

    Ok now I've finished adding equals/hash to the nodes

--- src/shared/ParseTreeParent.java
+++ src/shared/ParseTreeParent.java
@@ -2,6 +2,7 @@ package shared;
 
 import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 
 public class ParseTreeParent implements ParseTreeNode {
@@ -24,6 +25,21 @@ public class ParseTreeParent implements ParseTreeNode {
 
     public Set<List<ParseTreeNode>> getChildTrees() {
         return childTrees;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if(! (other instanceof ParseTreeParent)) {
+            return false;
+        }
+        ParseTreeParent otherParent = (ParseTreeParent) other;
+        return otherParent.nonterminal.equals(nonterminal) &&
+                otherParent.childTrees.equals(childTrees);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nonterminal, childTrees);
     }
 
     public String toString() {
